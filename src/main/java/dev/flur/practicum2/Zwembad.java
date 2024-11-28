314e094d78f0c4f3e9a77e9d517999e93eac4a03
flurbudurbur <sebastianhoogendijk@gmail.com>
2024-11-28 13:42:54 +0100

    Practicum 2A - Poging 2

--- src/main/java/dev/flur/practicum2/Zwembad.java
+++ src/main/java/dev/flur/practicum2/Zwembad.java
@@ -5,6 +5,20 @@ public class Zwembad {
     private double lengte;
     private double diepte;
 
+    public Zwembad(double breedte, double lengte, double diepte) {
+        this.breedte = breedte;
+        this.lengte = lengte;
+        this.diepte = diepte;
+    }
+
+    public Zwembad() {
+        this(0.0, 0.0, 0.0);
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
     public void setLengte(double lengte) {
         this.lengte = lengte;
     }
@@ -13,37 +27,24 @@ public class Zwembad {
         this.diepte = diepte;
     }
 
-    public void setBreedte(double breedte) {
-        this.breedte = breedte;
-    }
-
     public double getDiepte() {
         return diepte;
-    }
-
-    public double getLengte() {
-        return lengte;
     }
 
     public double getBreedte() {
         return breedte;
     }
 
+    public void setBreedte(double breedte) {
+        this.breedte = breedte;
+    }
+
     public double inhoud() {
         return breedte * lengte * diepte;
     }
 
-    public Zwembad() {
-        this(0.0, 0.0, 0.0);
-    }
-
+    @Override
     public String toString() {
         return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
-    }
-
-    public Zwembad(double breedte, double lengte, double diepte) {
-        this.breedte = breedte;
-        this.lengte = lengte;
-        this.diepte = diepte;
     }
 }
